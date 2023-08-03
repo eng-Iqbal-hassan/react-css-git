144fa1160d7c720c274730c7fa6b965b7f28d91f
eng-iqbal-hassan <eng.iqbal110@gmail.com>
2023-08-03 13:16:00 +0500

    first-commit

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export {default as Navbar} from './navbar/Navbar';
-export {default as Article} from './article/Article';
-export {default as Brand} from './brand/Brand';
-export {default as Featured} from './featured/Featured';
-export {default as CTA} from './cta/CTA';
